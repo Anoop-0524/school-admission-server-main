74c5cb3cdf4d53851dd9e66c9e30abbf15cc40e3
Anoop-0524 <anoopthomas333.at@gmail.com>
2024-11-27 01:54:38 +0530

    change config

--- config/db.config.js
+++ config/db.config.js
@@ -1,16 +1,13 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "Anoop@2405",
-    DB: "school-admission-forms",
-    // HOST: "eu-cdbr-west-02.cleardb.net",
-    // USER: "b52732cec0a226",
-    // PASSWORD: "9b66ce7a",
-    // DB: "heroku_8bd35375eec2376",
-    // HOST: "roundhouse.proxy.rlwy.net",
+    // HOST: "localhost",
     // USER: "root",
-    // PASSWORD: "c4CDcH11B-aEEe6bd1dHF1DGEDf34G62",
-    // DB: "railway",
+    // PASSWORD: "Anoop@2405",
+    // DB: "school-admission-forms",
+
+    HOST: "193.203.184.150",
+    USER: "u993427814_momentseventan",
+    PASSWORD: "Moment@1234",
+    DB: "u993427814_admission",
     PORT: 3306
 }
 
